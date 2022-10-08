86851f3cc536672e68f78a3da0c3875e827e54a1
Lance Vadla <lance.vadla@pexip.com>
2022-10-08 17:11:55 +0200

    feat: add operators and operands to output, all clear functionality

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,47 @@
-function helloWorld() {
-  const root = document.getElementById("root");
-  if (root) root.textContent = "hello world";
+const operands = document.querySelectorAll("[data-operand]");
+const operators = document.querySelectorAll("[data-operator]");
+const output = document.querySelector("[data-control='output']");
+const clear = document.querySelector("[data-control='clear']");
+
+operands.forEach((operand) => {
+  operand.addEventListener("click", () => operandListener(operand));
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", () => operatorListener(operator));
+});
+
+clear?.addEventListener("click", () => {
+  if (output && output.textContent) {
+    output.textContent = "";
+  }
+});
+
+function operandListener(el: Element) {
+  if (output && el instanceof HTMLElement) {
+    const operand = el.dataset.operand;
+    if (output.textContent) {
+      const dot = ".";
+      const lastOperand = trimOutputToFinalOperand(output.textContent);
+      const isFraction = !!lastOperand.includes(dot);
+      if (isFraction && operand === dot) return;
+    }
+    output.textContent += `${operand}`;
+  }
 }
 
-helloWorld();
+function operatorListener(el: Element) {
+  if (output && el instanceof HTMLElement) {
+    const operator = el.dataset.operator;
+    if (output.textContent) {
+      const lastOperand = trimOutputToFinalOperand(output.textContent);
+      if (lastOperand.length > 0) {
+        output.textContent += `${operator}`;
+      }
+    }
+  }
+}
+
+function trimOutputToFinalOperand(str: string): string {
+  return str.replace(/.*(\/|\*|\-|\+|\=)/, "");
+}
